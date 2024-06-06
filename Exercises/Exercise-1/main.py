e846a8e890901e40396074eb635db33baa564754
Yousuf Elkhoga <yousufelkhoga@yousufs-air.mynetworksettings.com>
2024-06-05 21:32:03 -0400

    e1 complete

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
+from sys import path
 import requests
+import os
+from zipfile import ZipFile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -13,8 +16,33 @@ download_uris = [
 
 def main():
     # your code here
-    pass
+    if not os.path.exists('./downloads'):
+        os.makedirs('./downloads')
+    
+
+    for url in download_uris:
+        zip_file_name = url.split('.com/')[1]
+        file_path = f"./downloads/{zip_file_name}"
+
+        try:
+            r = requests.get(url=url)
+            r.raise_for_status()
+        except requests.exceptions.RequestException as e: 
+            print(f"{e}")
+        
+        if r and r.ok:
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+            with open(file_path, 'wb') as file:
+                file.write(r.content)
+
+            with ZipFile(file_path, 'r') as zip:
+                zip.extractall('./downloads')
+
+        else:
+            print('zip file download not successful')
+
 
 
 if __name__ == "__main__":
-    main()
+    main()``
